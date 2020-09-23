8a92c29426566df8f45c5d39cd9ef1756353807b
Gabriel Stancu <gabriel.stancu07@yahoo.com>
2020-09-23 13:43:09 +0300

    project files upload

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        Debug.Log("Welcome to Number Wizard, wanderer");
+        Debug.Log("Pick a number if you dare...");
+        Debug.Log("The biggest number you can choose is: " + max);
+        Debug.Log("The smallest number you can choose is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Push Up = Higher, Down = Lower, Enter = Correct");
+        max += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I am a genius wizard.");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higer or lower than " + guess + "?");
+    }
+}
